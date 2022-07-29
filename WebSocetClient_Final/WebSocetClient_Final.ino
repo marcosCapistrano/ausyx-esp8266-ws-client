26147d5c0400083ac25a8022581e6248cfd342ee
Marcos Capistrano <mcapistrano50@gmail.com>
2022-07-29 15:46:38 -0300

    Funcionando WebSocket 100%!

--- WebSocetClient_Final/WebSocetClient_Final.ino
+++ WebSocetClient_Final/WebSocetClient_Final.ino
@@ -1,126 +1,74 @@
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
 #include <WebSocketsClient.h>
 
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+ESP8266WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
+
 #define oneWireBus 0
 
-const char* ssid     = "CT_101";
-const char* password = "AusyxSolucoes";
-
-WiFiEventHandler handleStationModeConnected;
-WiFiEventHandler handleStationModeDisconnected;
-WiFiEventHandler handleStationModeGotIP;
-
-WebSocketsClient wsClient;
-void handleWSEvent(WStype_t type, uint8_t * payload, size_t length);
-
-void wifi_connect(void);
-void ws_connect(void);
-void ws_disconnect(void);
-
-bool wifiConnected = false;
-bool wsConnected = false;
-
-float temperatura = 0.0;
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length);
 OneWire oneWire(oneWireBus);
 DallasTemperature sensors(&oneWire);
 
 void setup() {
   Serial.begin(115200);
+  WiFiMulti.addAP("CT_101", "AusyxSolucoes");
 
-  WiFi.mode(WIFI_STA);
-  handleStationModeConnected = WiFi.onStationModeConnected(&onWiFiConnected);
-  handleStationModeDisconnected = WiFi.onStationModeDisconnected(&onWiFiDisconnected);
-  handleStationModeGotIP = WiFi.onStationModeGotIP(&onWiFiGotIP);
+  while(WiFiMulti.run() != WL_CONNECTED) {
+    delay(100);
+  }
 
-  wsClient.onEvent(webSocketEvent);
-  wsClient.enableHeartbeat(10000, 3000, 3);
-  
-  wifi_connect();
+  webSocket.begin("192.168.4.1", 80, "/");
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setReconnectInterval(5000);
+  webSocket.enableHeartbeat(15000, 3000, 2);
 }
+
 
 //loopCounter será usado para toda vez que o loop rodar 15 vezes, a temperatura seja enviada
 // O que da mais ou menos 10 segundos
 int loopCounter = 0;
+int temperatura = 0;
+char tempStr[3];
 
 void loop() {
-  if (wifiConnected) {
-    if (wsConnected) {
-      wsClient.loop();
+  webSocket.loop();
 
-      if (loopCounter >= 15) {
+  if(webSocket.isConnected()) {
+    if (loopCounter >= 50) {
         sensors.requestTemperatures();
         temperatura = sensors.getTempCByIndex(0);
 
-        temperatura = (temperatura * 100) / 100;
-
         if (temperatura < 1) temperatura = 0;
-        if (temperatura > 99) temperatura = 99;
+        if (temperatura > 150) temperatura = 150;
 
+        itoa(temperatura, tempStr, 10);
+      
         Serial.println(" ");
         Serial.print("Temperatura: ");
-        Serial.println(temperatura);
+        Serial.println(tempStr);
 
-        wsClient.sendTXT(temperatura);
+        webSocket.sendTXT(tempStr);
         loopCounter = 0;
       }
-    }
   }
 
   loopCounter++;
-  delay(150);
-}
-
-void wifi_connect() {
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-}
-
-void ws_connect() {
-  Serial.println("Attempting to connect to WebSocket");
-  wsClient.begin("192.168.4.1", 80, "ws");
-}
-
-void ws_disconnect() {
-  wsClient.disconnect();
-}
-
-void onWiFiConnected(const WiFiEventStationModeConnected &evt) {
-  Serial.println("WiFi connected!");
-}
-
-void onWiFiDisconnected(const WiFiEventStationModeDisconnected &evt) {
-  Serial.println("WiFi disconnected!");
-  wifiConnected = false;
-  wsConnected = false;
-
-  wifi_connect();
-  ws_disconnect();
-}
-
-void onWiFiGotIP(const WiFiEventStationModeGotIP &evt) {
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  //connect web socket
-  ws_connect();
-  wifiConnected = true;
+  delay(100);
 }
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
-  switch (type) {
+  switch(type) {
     case WStype_DISCONNECTED:
-      Serial.println("WebSocket Disconnected!");
-      wsConnected = false;
-      ws_connect();
+      Serial.printf("[WSc] Disconnected!\n");
       break;
     case WStype_CONNECTED:
-      Serial.printf("WebSocket Connected to url: %s\n", payload);
-      wsConnected = true;
+      Serial.printf("[WSc] Connected to url: %s\n", payload);
       break;
-  }
+    }
 }
